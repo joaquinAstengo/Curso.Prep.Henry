e760ded6367514a55f4995984aed5801051d1aeb
joaquinAstengo <joaquin.astengo@gmail.com>
2020-12-26 17:04:48 -0300

    JSII-homerork

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -83,7 +83,7 @@ function fizzBuzz(numero) {
     } else if (numero % 5 === 0){
       return "buzz"
   } else {
-    return numero;
+    return num
   } 
 }
 
@@ -93,16 +93,15 @@ function esPrimo(numero) {
   // Pista: un número primo solo es divisible por sí mismo y por 1
   // Pista 2: Puedes resolverlo usando un bucle `for`
   // Nota: Los números 0 y 1 NO son considerados números primos
-  if( numero < 2) return false;
+  if(numero < 2) return false;
   if(numero === 2) return true;
-  for(var i = 2; i < numero; i++) {
+  for(let i = 2; i < numero; i++) {
     if(numero % i === 0) {
       return false;
     }
   }
   return true;
 }
-
 
 // No modificar nada debajo de esta línea
 // --------------------------------
